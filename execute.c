bc68dee316640e58a20673eb4bc516318e318601
JFFPtech <arrozconsalchichas1229@gmail.com>
2024-01-03 09:49:04 -0400

    fixed all compilation errors for these files

--- execute.c
+++ execute.c
@@ -7,40 +7,35 @@
  * @counter: Line number in the Monty file
  * @file: Pointer to the Monty file
  */
-void execute(char *line, stack_t **stack, unsigned int counter, FILE *file)
+int execute(char *line, stack_t **stack, unsigned int counter, FILE *file)
 {
-    char *opcode;
-    int value;
+	instruction_t opst[] = {
+				{"push", f_push}, {"pall", f_pall}, {"pint", f_pint},
+				{"pop", f_pop},
+				{"swap", f_swap},
+				{"nop", f_nop},
+				{NULL, NULL}
+				};
+	unsigned int i = 0;
+	char *op;
 
-    /*Tokenize the command line to get the opcode and value*/
-    opcode = strtok(line, " \t\n");
-    if (opcode == NULL || opcode[0] == '#')
-        return;
-
-    if (strcmp(opcode, "push") == 0)
-    {
-        char *arg = strtok(NULL, " \t\n");
-        if (arg == NULL)
-        {
-            fprintf(stderr, "L%d: usage: push integer\n", counter);
-            fclose(file);
-            free_stack(*stack);
-            exit(EXIT_FAILURE);
-        }
-
-        value = atoi(arg);
-        push(stack, value);
-    }
-    else if (strcmp(opcode, "pall") == 0)
-    {
-        pall(*stack);
-    }
-    else
-    {
-        fprintf(stderr, "L%d: unknown instruction %s\n", counter, opcode);
-        fclose(file);
-        free_stack(*stack);
-        exit(EXIT_FAILURE);
-    }
+	op = strtok(line, " \n\t");
+	if (op && op[0] == '#')
+		return (0);
+	bus.arg = strtok(NULL, " \n\t");
+	while (opst[i].opcode && op)
+	{
+		if (strcmp(op, opst[i].opcode) == 0)
+		{	opst[i].f(stack, counter);
+			return (0);
+		}
+		i++;
+	}
+	if (op && opst[i].opcode == NULL)
+	{ fprintf(stderr, "L%d: unknown instruction %s\n", counter, op);
+		fclose(file);
+		free(line);
+		free_stack(*stack);
+		exit(EXIT_FAILURE); }
+	return (1);
 }
-
