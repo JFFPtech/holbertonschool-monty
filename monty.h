33ea0ce2065c6a1988e15b2e7180b63059c03164
JFFPtech <arrozconsalchichas1229@gmail.com>
2024-01-03 15:01:54 -0400

    prototype for addnode and updated monty.h

--- monty.h
+++ monty.h
@@ -78,7 +78,9 @@ void f_pall(stack_t **head, unsigned int number);
 void f_pint(stack_t **head, unsigned int number);
 void f_pop(stack_t **head, unsigned int counter);
 void f_swap(stack_t **head, unsigned int counter);
+void f_add(stack_t **head, unsigned int counter);
 void f_nop(stack_t **head, unsigned int counter);
+void addnode(stack_t **head, int n);
 
 
 #endif
