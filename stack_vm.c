ba26493a5085c0869da9725a6191c9dd572df95d
JFFPtech <arrozconsalchichas1229@gmail.com>
2024-01-03 09:00:47 -0400

    added main interpreter function to stack

--- stack_vm.c
+++ stack_vm.c
@@ -7,29 +7,60 @@ int stack[STACK_SIZE];
 int sp = -1;
 
 void push(int value, int line_number) {
-    if (stack.top >= STACK_SIZE) {
-        fprintf(stderr, "L%d: Stack overflow\n", line_number);
-        exit(EXIT_FAILURE);
-    }
-    stack.stack[++stack.top] = value;
+	if (sp == STACK_SIZE - ) {
+		fprintf(stderr, "L%d: Stack overflow\n", line_number);
+		exit(EXIT_FAILURE);
+	}
+	stack[++sp] = value;
 }
 
 void pall() {
-    int i;
-    for (i = stack.top; i > 0; i--) {
-        printf("%d\n", stack.stack[i]);
-    }
+	for (int i = sp; i > 0; i--) {
+		printf("%d\n", stack[i]);
+	}
+}
+
+void interpret(FILE *file) {
+	char line[100];
+	int line_number = 0;
+
+	while (fgets(line, sizeof(line), file) != NULL) {
+		line_number++;
+
+	char opcode[10];
+	int value;
+
+	if (sscanf(line, "%s %d", opcode, &value) == 2) {
+	if (strcmp(opcode, "push") == 0) {
+		push(value, line_number);
+		} else if (strcmp(opcode, "pall") == 0) {
+			pall();
+		} else {
+			fprintf(stderr, "L%d: Unknown opcode: %s\n", line_number, opcode);
+			exit(EXIT_FAILURE);
+		}
+	} else if (strcmp(opcode, "pall") != 0) {
+		fprintf(stderr, "L%d: Invalid line: %s", line_number, line);
+		exit(EXIT_FAILURE);
+	} else {
+		}
+	}
 }
 
 int main(int argc, char *argv[]) {
-    // Initialize stack
-    stack.top = 0;
+	if (argc != 2) {
+		fprintf(stderr, "Usage: %s <filename>\n", argv[0]);
+		exit(EXIT_FAILURE);
+	}
 
-    // Sample bytecode execution
-    push(1, 1);
-    push(2, 2);
-    push(3, 3);
-    pall();
+	FILE *file = fopen(argv[1], "r");
+	if (file == NULL) {
+		perror("Error opening file");
+		exit(EXIT_FAILURE);
+	}
 
-    return 0;
+	interpret(file);
+
+	fclose(file);
+	return 0;
 }
