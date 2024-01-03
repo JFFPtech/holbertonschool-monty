dcca04953e1f649cff56949a9e8d81eafae30e1e
JFFPtech <arrozconsalchichas1229@gmail.com>
2024-01-03 15:20:06 -0400

    included f_stack function

---
+++ stack.c
@@ -0,0 +1,14 @@
+#include "monty.h"
+/**
+ * f_stack - prints the top of the stack
+ * @head: stack head
+ * @counter: counts the lines
+ * Return: none
+ */
+
+void f_stack(stack_t **head, unsigned int counter)
+{
+	(void)head;
+	(void)counter;
+	bus.lifi = 0;
+}
