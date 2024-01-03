70dd07050fb6f82680eaefb2b3a0471f7fadd3e0
scardona14 <cardanasean123@gmail.com>
2024-01-02 22:20:49 -0400

    push,pall

---
+++ stack_vm.c
@@ -0,0 +1,39 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+#define STACK_SIZE 100
+
+typedef struct {
+    int stack[STACK_SIZE];
+    int top;
+} Stack;
+
+Stack stack;
+
+void push(int value, int line_number) {
+    if (stack.top >= STACK_SIZE) {
+        fprintf(stderr, "L%d: Stack overflow\n", line_number);
+        exit(EXIT_FAILURE);
+    }
+    stack.stack[++stack.top] = value;
+}
+
+void pall() {
+    int i;
+    for (i = stack.top; i > 0; i--) {
+        printf("%d\n", stack.stack[i]);
+    }
+}
+
+int main(int argc, char *argv[]) {
+    // Initialize stack
+    stack.top = 0;
+
+    // Sample bytecode execution
+    push(1, 1);
+    push(2, 2);
+    push(3, 3);
+    pall();
+
+    return 0;
+}
